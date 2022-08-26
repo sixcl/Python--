b8f89659b97aa5251beffa697eef7057f05172bd
sixcl <123456123@qq.com>
2022-08-26 21:01:11 +0800

    牛客网python基础

---
+++ step1/题目5.py
@@ -0,0 +1,13 @@
+'''
+描述
+牛牛、牛妹和牛可乐正在Nowcoder学习Python语言，现在给定他们三个当中的某一个名字name，
+假设输入的name为Niuniu，则输出 I am Niuniu and I am studying Python in Nowcoder!
+请按以上句式输出相应的英文句子。
+输入描述：
+一行一个字符串表示名字。
+输出描述：
+假设输入的name为Niuniu，则输出I am Niuniu and I am studying Python in Nowcoder!
+请按以上句式输出相应的英文句子。
+'''
+name = input()
+print(f'I am {name} and I am studying Python in Nowcoder!')
---
+++ step2/题目8.py
@@ -0,0 +1,8 @@
+'''
+输入描述：
+输入一个整数。
+输出描述：
+输出强制类型转换后的结果，换行再输出变量类型。
+'''
+num = float(input())
+print(float(num),type(num),sep="\n")
